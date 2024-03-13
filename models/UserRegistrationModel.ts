dd98a53411174a124f3ddc885cb1bdbb68a9693b
sebastian-mcpe <78837195+sebastian-mcpe@users.noreply.github.com>
2024-03-13 02:53:40 -0400

    feat: Created User registration model

---
+++ models/UserRegistrationModel.ts
@@ -0,0 +1,10 @@
+class UserRegistrationModel {
+    name: string;
+    email: string;
+    password: string;
+    constructor(name: string, email: string, password: string) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
